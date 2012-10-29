6f8a9ebd1b9e2faf5ac4da3e722d5a678ca96ef6
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-10-29 18:47:47 +0000

    Moved carelisting schemas from virtualservices to hval24 component

---
+++ GetListing/Hval24transform/src/test/java/se/skl/crm/carelisting/CarelistingIntegrationTest.java
@@ -0,0 +1,69 @@
+package se.skl.crm.carelisting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.transport.http.HttpConnector;
+
+import se.skl.riv.crm.carelistingresponder.v1.getlisting.GetListingResponseType;
+
+public class CarelistingIntegrationTest extends FunctionalTestCase
+{
+    private String urlCarelist = null;
+    private String urlHval = null;
+
+	public CarelistingIntegrationTest() {
+		ResourceBundle rb = ResourceBundle.getBundle("hval");
+		urlCarelist = rb.getString("carelisting.url");
+		urlHval = rb.getString("hvalriks.url");
+	}
+	
+	@Override
+	protected String getConfigResources()
+	{
+		return "carelisting-unittest-config.xml";
+	}
+	
+    public void testNationelVirtualService() throws Exception
+    {
+    	String personId = "19621008-3611";
+		Consumer consumer = new Consumer(urlCarelist);
+		GetListingResponseType reply = consumer.callService(personId);
+		assertEquals(personId, reply.getSubjectOfCare().getPersonId());
+	}
+	
+    public void testBackendService() throws Exception
+    {
+    	String personId = "196210083611";
+    	MuleClient client = new MuleClient();
+    	Map<String, Object> props = new HashMap<String, Object>();
+        props.put(HttpConnector.HTTP_METHOD_PROPERTY, "GET");
+        MuleMessage result = client.send(urlHval + "/" + personId, "", props);
+        String res = convertStreamToString((InputStream)result.getPayload());
+        assertTrue(res.indexOf(personId) != -1);
+        assertTrue(res.indexOf("Kalle") != -1);
+        assertTrue(res.indexOf("LAN X") != -1);
+    }
+   
+	private String convertStreamToString(InputStream is) {
+        try {
+			    StringBuilder sb = new StringBuilder();
+			    String line;
+		        BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+		        while ((line = reader.readLine()) != null) {
+		            sb.append(line).append("\n");
+		        }
+			    return sb.toString();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+    }
+}
