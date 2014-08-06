2280b2b069b8b105461500b9b8a34cd1e7f01730
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2014-08-06 12:28:59 +0000

    MIG-141: Implementera PingFor Configuration i samtliga tjänster ----------------------------------------------------------- Centrala ändringar i denna incheckning:
    -HvalAdapterIC/pom.xml, beroende till tjänstekontrakt för itintegration monitoring
    -PingForconfigurationService.java, PingForConfiguration-rivtabp21-service.xml, PingForconfigurationServiceIntegrationTest
    -Hval24Adapter-config.properties, nya properties
      # PingForConfiguration properties
      PINGFORCONFIGURATION_INBOUND_ENDPOINT=http://localhost:11001/tb/itintegration/monitoring/PingForConfiguration/1/rivtabp21
      APPLICATION_NAME=Hval24Adapter
    -Hval24Adapter-soapui-project.xml
    
    Övriga ändringar kommer från en mvn license:format som behövdes för att få in korrekt licenseinformation i källkodsfilerna

--- Hval24AdapterIC/src/main/java/se/skl/tp/vp/util/helper/PayloadHelper.java
+++ Hval24AdapterIC/src/main/java/se/skl/tp/vp/util/helper/PayloadHelper.java
@@ -1,8 +1,5 @@
 /**
- * Copyright (c) 2013 Center for eHalsa i samverkan (CeHis).
- * 							<http://cehis.se/>
- *
- * This file is part of SKLTP.
+ * Copyright (c) 2014, Inera AB <http://www.inera.se/>. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,7 +13,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
  */
 package se.skl.tp.vp.util.helper;
 
